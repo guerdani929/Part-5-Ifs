f93d4a768c22ee472e8d242a54ba4a09b4e4519e
guerdani929 <GuerDani929@GoTVDSB.ca>
2020-09-16 10:03:40 -0400

    Add project files.

---
+++ Part 5 Ifs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5_Ifs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string age;
+
+
+            Console.Write("Please enter your age: ");
+            age = Console.ReadLine();
+
+
+
+            if int.TryParse(age, )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
